dac1241a780c706ccdf98313d72d314e14d5b1ce
shivamkumarsingh98 <shiavmkumarsingh.chu98@gmail.com>
2024-03-13 08:44:11 +0530

    mobile responsive

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -9,10 +9,10 @@ function Header() {
           className="w-screen h-screen bg-no-repeat bg-cover -z-30  "
         />
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-        <div className="absolute top-[150px] left-10 text-white font-serif text-[4rem] font-extrabold ">
+        <div className="absolute top-[150px] left-10 text-white font-serif lg:text-[4rem] sm:text-[3rem] md:text-left md:text-[4rem] font-extrabold lg:text-left sm:text-center  ">
           <p>Apno ko Apno </p>
           <p>Se Milaye</p>
-          <span className="text-red-600 font-serif font-extrabold">
+          <span className="text-red-600 font-serif font-extrabold lg:text-left sm:text-center">
             <Typewriter
               words={["Milan"]}
               loop={1000}
--- src/component/navebar/Navebar.jsx
+++ src/component/navebar/Navebar.jsx
@@ -21,7 +21,7 @@ function Navebar() {
           <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
               <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                   {/* Mobile menu button*/}
                   <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="absolute -inset-0.5" />
@@ -33,13 +33,13 @@ function Navebar() {
                     )}
                   </Disclosure.Button>
                 </div>
-                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
                   <div className="flex flex-shrink-0 items-center">
                     <h1 className="text-red-600 text-[25px] font-serif font-extrabold">
                       Milan
                     </h1>
                   </div>
-                  <div className="hidden sm:ml-6 sm:block">
+                  <div className="hidden md:ml-6 md:block">
                     <div className="flex space-x-4">
                       {navigation.map((item) => (
                         <a
@@ -59,7 +59,7 @@ function Navebar() {
                     </div>
                   </div>
                 </div>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
                   <button
                     type="button"
                     className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
