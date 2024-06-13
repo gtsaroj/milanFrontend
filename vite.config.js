ee51d0a91fd1778e938aebd5be0cf5c3b8e834eb
gtsaroj <sarojgt326@gmail.com>
2024-06-13 09:59:52 +0545

    update server url

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          "http://ec2-13-202-12-148.ap-south-1.compute.amazonaws.com:8000/",
-        changeOrigin: true,
-      },
-    },
-  },
 });
